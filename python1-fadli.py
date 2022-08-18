c938384e55e951c2cd105d061c602cab80a8e61b
fadliwork55 <110509608+fadliwork55@users.noreply.github.com>
2022-08-18 14:01:30 +0700

    Add files via upload

---
+++ python1-fadli.py
@@ -0,0 +1,9 @@
+print("Pesan Kalian")
+print("Selamat Datang Fadli")
+print("Saya akan memperkenalkan tentang diri saya sendiri")
+print("Saya sering dipanggil jangkung")
+print("Cita cita saya menjadi pendek")
+print("Saya tidak suka pedes")
+print("Teman teman saya pendek semua")
+print("Saya pernah nagis")
+# Buat 10 baris pesan
---
+++ python2-fadli.py
@@ -0,0 +1,22 @@
+
+nama_depan = "Fadli"
+print("Nama depan anda adalah",nama_depan)
+
+
+
+# Soal Latihan
+# Buatlah 2 variabel yang berisi
+# nama depan dan nama belakang
+# lalu tampilkan keduanya
+
+nama_depan = "Fadli" #Variabel Pertama
+#variabel ke-2
+print(???)
+
+
+
+# String petik satu ('')
+#       petik dua (" ")
+# Integer contoh: 1, 10, 15, 30
+# Float contoh: 1.0, 20.5, 15.75
+# Boolean contoh: True / False
